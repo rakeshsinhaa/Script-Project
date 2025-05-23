486169cc6225b8314d4bb6455c40a77f6de17dec
Rakesh Sinha <sinharakesh078@gmail.com>
2025-05-23 10:48:58 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StoryInput from "./components/StoryInput";
 import ScriptViewer from "./components/ScriptViewer";
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <Router>
+        <Navbar/>
       <div className="min-h-screen bg-gray-50 p-6">
         <h1 className="text-4xl text-center font-bold mb-6">StoryScript Generator</h1>
         <Routes>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
-function navbar() {
-  return (
-    <div>
-      csd
-    </div>
-  )
-}
+import { Link } from "react-router-dom";
 
-export default navbar
+const Navbar = () => {
+  return (
+    <nav className="bg-purple-700 text-white  px-6 py-4 rounded-b-4xl shadow-md">
+      <div className="flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/" className="text-2xl font-bold">
+          StoryScript
+        </Link>
+        <div className="space-x-4">
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
+          <Link to="/script-viewer" className="hover:underline">
+            View Script
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
