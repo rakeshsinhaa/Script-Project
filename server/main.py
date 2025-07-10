7a76e24cc6be86863406ee0404630d80955ae963
AbhishekSinghDIkhit <aabhishek576@gmail.com>
2025-07-10 20:49:59 +0530

    update

--- server/main.py
+++ server/main.py
@@ -45,10 +45,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://script-project.vercel.app",
-        "http://localhost:3000"
-    ],
+    allow_origins=["*"],
+    # allow_origins=[
+    #     "https://script-project.vercel.app",
+    #     "http://localhost:3000"
+    # ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
